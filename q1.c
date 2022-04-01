8047734478a5573fd82967369b114a23548a2705
yashwanth BELLALA <94678112+yashva93327@users.noreply.github.com>
2022-04-01 11:50:16 +0000

    Done

--- q1.c
+++ q1.c
@@ -1,29 +1,29 @@
-#include<stdio.h>
-int input(){
+#include <stdio.h>
+int input() {
   int n;
   printf("enter the number\n");
-  scanf("%d",&n);
+  scanf("%d", &n);
   return n;
 }
-int composite(int n){
-  int value=0;
-  for(int i=1;i<=n/2;i++){
-    if(n%i==0)
-      value=value + 1;
+int composite(int n) {
+  int value = 0;
+  for (int i = 1; i <= n / 2; i++) {
+    if (n % i == 0)
+      value = value + 1;
     else
-      value=value;
+      value = value;
   }
   return value;
 }
-void output(int n,int composite){
-  if(composite>1)
-    printf("%d is composite\n",n);
+void output(int n, int composite) {
+  if (composite > 1)
+    printf("%d is composite\n", n);
   else
-    printf("%d is prime\n",n);
+    printf("%d is prime\n", n);
 }
-int main(){
-  int n=input();
-  int p=composite(n);
-  output(n,p);
+int main() {
+  int n = input();
+  int p = composite(n);
+  output(n, p);
   return 0;
-  }
+}
