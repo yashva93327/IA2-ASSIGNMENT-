4be33b5c5253ebdf6f18858b9e92024df813c81e
yashwanth BELLALA <94678112+yashva93327@users.noreply.github.com>
2022-04-02 06:00:50 +0000

    done.

---
+++ q16.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  float pi=3.14,r,ar;
+  printf("enter the radius of a circle\n");
+  scanf("%f",&r);
+  ar=pi*r*r;
+  printf("the area of circle is %f\n",ar);
+  return 0;
+  
+}
