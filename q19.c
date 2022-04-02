efef7bbb69d9c9cc08801295d5e994787a6bb786
yashwanth BELLALA <94678112+yashva93327@users.noreply.github.com>
2022-04-02 06:09:05 +0000

    done.

---
+++ q19.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  float pi=3.24,r,ar;
+  printf("enter the radius of circle\n");
+  scanf("%f",&r);
+  ar=4*pi*r*r;
+  printf("the area of sphere is %f\n",ar);
+  return 0;
+}
